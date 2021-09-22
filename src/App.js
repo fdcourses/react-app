4cdc32dd124f27593e102d8e38da6a60ef635dad
Kirill <freshjs.ftd@gmail.com>
2021-09-22 15:20:23 +0300

    Add incomplete StopWatch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { Component } from 'react';
-import AlohaDashboard from './components/AlohaDashboard';
+// import AlohaDashboard from './components/AlohaDashboard';
+import StopWatch from './components/StopWatch';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible: true,
+    };
+  }
+
+  toggleVisibility = () => {
+    this.setState({
+      isVisible : !this.state.isVisible
+    })
+  }
+
   render() {
-    return <AlohaDashboard />;
+    const { isVisible } = this.state;
+
+    return (
+      <>
+        {isVisible && <StopWatch />}
+        <button onClick={this.toggleVisibility}>Hide</button>
+      </>
+    );
   }
 }
 
