7cd2d431e91b32748cd232b828249e71fe8ed7ba
Kirill <freshjs.ftd@gmail.com>
2021-10-06 15:14:46 +0300

    Add useEffect

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Component } from 'react';
 import CONSTANTS from './constants';
 import Counter from './components/Counter';
 import MouseTracker from './components/MouseTracker';
 const { THEMES } = CONSTANTS;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App(props) {
+  const [theme, setTheme] = useState(THEMES.DARK);
+  const [user, setUser] = useState({
+    id: 1,
+    fullName: 'User Userovich',
+    userImg:
+      'https://www.ikea.com/kr/en/images/products/jaettelik-soft-toy-dinosaur-dinosaur-velociraptor__0814802_pe772713_s5.jpg?f=undefined',
+  });
+  const [isVisible, setIsVisible] = useState(true);
 
-    this.state = {
-      theme: THEMES.DARK,
-      user: {
-        id: 1,
-        fullName: 'User Userovich',
-        userImg:
-          'https://www.ikea.com/kr/en/images/products/jaettelik-soft-toy-dinosaur-dinosaur-velociraptor__0814802_pe772713_s5.jpg?f=undefined',
-      },
-    };
-  }
-
-  setTheme = (newTheme) => {
-    this.setState({ theme: newTheme });
-  };
-
-  render() {
-    const { theme, user } = this.state;
-    return (
-      <>
-        <Counter />
-        <MouseTracker />
-      </>
-    );
-  }
+  return (
+    <>
+      <button onClick={() => setIsVisible(!isVisible)}>Switch</button>
+      {isVisible && <MouseTracker />}
+    </>
+  );
 }
 
 export default App;
--- src/components/MouseTracker/index.jsx
+++ src/components/MouseTracker/index.jsx
@@ -1,28 +1,73 @@
-import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, { useState, useEffect } from 'react';
 
 const MouseTracker = (props) => {
   const [coordinates, setCoordinates] = useState({
     x: 0,
-    y: 0
+    y: 0,
   });
+
+  const [counter, setCounter] = useState(0);
+  const [step, setStep] = useState(1);
 
   const mouseMoveHandler = (event) => {
     setCoordinates({
       x: event.clientX,
-      y: event.clientY
+      y: event.clientY,
     });
+  };
+  const clickHandler = () => {
+    console.log('я работаю');
+    setCounter(function (prevCounter) {
+      return prevCounter + step;
+    });
+  };
+
+  useEffect(function effects() {
+    // console.log('useEffect');
+    document.body.addEventListener('click', clickHandler);
+
+    return function componentWillUnmount() {
+      console.log('clear effects');
+      document.body.removeEventListener('click', clickHandler);
+    };
+  }, [step]);
+
+  // console.log('test');
+
+  const handleChange = ({target: {value}}) => {
+    setStep(Number(value));
   }
 
-  
-  // document.body.addEventListener('mousemove', mouseMoveHandler);
-
   return (
-    <div style={{height: '100vh'}} onMouseMove={mouseMoveHandler}>
+    <div
+      style={{ height: '100vh' }}
+      onMouseMove={mouseMoveHandler}
+    >
       <p>x: {coordinates.x}</p>
       <p>y: {coordinates.y}</p>
+      <p>Count: {counter}</p>
+      <input value={step} onChange={handleChange} />
     </div>
   );
-}
+};
 
+const oldState = {
+  x: 123,
+  y: 324,
+  count: 5,
+};
+
+const newState = {
+  count: 6,
+};
+
+// классовое обновление стейта
+let updatedState = {
+  ...oldState,
+  ...newState,
+};
+// на хуках
+updatedState = {
+  ...newState,
+};
 export default MouseTracker;
