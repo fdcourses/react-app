b44e0cf982e92335ad8a6ef424fc62d16e7ec1a7
Kirill <freshjs.ftd@gmail.com>
2021-10-07 17:05:01 +0300

    Add useMemo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,54 +1,51 @@
-import React, { useContext, useState, useCallback } from 'react';
+import React, { useContext, useState, useCallback, useMemo } from 'react';
 import cx from 'classnames';
 import styles from './Header.module.scss';
 import CONSTANTS from '../../constants';
 import { ThemeContext, UserContext } from '../../contexts';
-import { useEffect } from 'react/cjs/react.development';
 
 const { THEMES } = CONSTANTS;
 
+function computeValue(number) {
+  let  i = 0;
+
+  for(let j = 0; j < 1_000_000_000; j++) {
+    i++;
+  }
+
+  return number ** 2;
+}
+
 const Header = (props) => {
   const [theme, setTheme] = useContext(ThemeContext);
-  const { fullName, userImg } = useContext(UserContext);
-  const [value, setValue] = useState(1);
+  const {fullName, userImg } = useContext(UserContext);
+  const [value, setValue] = useState(2);
 
   const headerClasses = cx(styles.container, {
     [styles.darkTheme]: theme === THEMES.DARK,
     [styles.lightTheme]: theme === THEMES.LIGHT,
   });
+  
+  const changeValue = (e) => setValue(Number(e.target.value));
 
-  // const logValue = () => {console.log(value)};
-  const logValue = useCallback(() => {
-    console.log(value);
-  }, [value]);
+  const changeTheme = () => setTheme(otherTheme);
 
-  useEffect(() => {
-    console.log('log value создался');
-  }, [logValue]);
+  // const newValue = computeValue(value);
 
-  const changeValue = useCallback(
-    (e) => setValue(Number(e.target.value)),
-    []
-  );
+  const newValue = useMemo(()=> {return computeValue(value)}, [value]);
 
-  useEffect(() => {
-    console.log('changeValue создался');
-  }, [changeValue]);
+  // useEffect(()=> {
+
+  // })
 
   const otherTheme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
-  const changeTheme = useCallback(() => setTheme(otherTheme), [otherTheme]);
-
-  useEffect(() => {
-    console.log('changeTheme создался');
-  }, [changeTheme]);
-
 
   return (
     <header className={headerClasses}>
-      <h1>Hello {fullName}</h1>
+      <h1>{newValue}</h1>
       <img src={userImg} alt="user pic" style={{ width: '60px' }} />
       <button onClick={changeTheme}>SWITHC THEME</button>
-      <button onClick={logValue}>log value</button>
+
       <input value={value} onChange={changeValue} />
     </header>
   );
