7d41848773ef12cbd8e307f149da77b5d65e10a9
Kirill <freshjs.ftd@gmail.com>
2021-10-12 17:12:55 +0300

    Add Formik form

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -1,66 +1,45 @@
 import React, { Component, useState } from 'react';
-import styles from './SignInForm.module.scss';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 
+import styles from './SignInForm.module.scss';
 import { SIGN_IN_SCHEMA } from 'utils/validationSchemas';
 
-const goodSumbit = {
-  email: 'andrey@mail.ru',
-  password: '1Q#e0000',
-  remember: 'remember',
-};
-
-const badSubmit = {
-  email: '21345test',
-  password: 'test@test.text',
-  remember: '',
+const initialState = {
+  email: '',
+  password: '',
 };
 
 function SignInForm(props) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isRemembering, setIsRemembering] = useState(false);
-  const handleEmail = ({ target: { value } }) => {
-    setEmail(value);
-  };
-  const handlePassword = ({ target: { value } }) => {
-    setPassword(value);
-  };
-  const handleRemember = () => {
-    setIsRemembering(!isRemembering);
+  const submitHandler = (values, formikBag) => {
+    console.log(values);
+    console.log(formikBag);
+
+    formikBag.resetForm();
   };
 
-  try {
-    console.log(SIGN_IN_SCHEMA.isValidSync(goodSumbit));
-    console.log(SIGN_IN_SCHEMA.validateSync(badSubmit));
-  } catch (error) {
-    console.log(error);
-  }
   return (
-    <form className={styles.container}>
-      <input
-        name="email"
-        value={email}
-        onChange={handleEmail}
-        placeholder="email"
-      />
-      <input
-        name="password"
-        value={password}
-        onChange={handlePassword}
-        placeholder="password"
-      />
-      <label>
-        <input
-          type="checkbox"
-          name="remember"
-          value={isRemembering ? 'remember' : ''}
-          checked={isRemembering}
-          onChange={handleRemember}
-        />
-        Запомнить меня
-      </label>
-      <button>Login</button>
-    </form>
+    <Formik
+      initialValues={initialState}
+      validationSchema={SIGN_IN_SCHEMA}
+      onSubmit={submitHandler}
+    >
+      {(formikProps) => {
+        console.log(formikProps);
+        return (
+          <Form className={styles.container}>
+            <Field name="email" placeholder="email" />
+            <ErrorMessage
+              name="email"
+              children={(msg) => <div style={{ color: 'red' }}>{msg}</div>}
+            />
+            <Field type="text" name="password" placeholder="password" />
+            <ErrorMessage name="password"  component='article'/>
+            <button type="submit">Login</button>
+            <button type="reset">Reset</button>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 }
 
--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,12 +1,12 @@
 import * as yup from 'yup';
 
 export const SIGN_IN_SCHEMA = yup.object({
-  email: yup.string().email().required(),
+  email: yup.string().email('мыло не правильное').required('введи эмейл плиз'),
   password: yup
     .string()
-    .min(8)
-    .max(32)
-    .matches(/^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[a-z])(?=.*?[$_#^!%]).{8,32}$/)
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[a-z])(?=.*?[$_#^!%]).{8,32}$/,
+      'Пароль должен содержать 8 - 32 симовла, заглавную, строчную буквы, число и спец символ'
+    )
     .required(),
-  remember: yup.string(),
 });
