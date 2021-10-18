293ac6035fb5fb3143d75be5516535867491a6d5
Kirill <freshjs.ftd@gmail.com>
2021-10-18 14:27:13 +0300

    Add currying example

--- sandbox.js
+++ sandbox.js
@@ -34,29 +34,78 @@
   напушите функцию которая будет генерировать числа от 1 до бесконечности. Каждый ваш вызов next увеличивает значение на 1
 */
 
-function* generateNumbers(maxNumber) {
-  try {
-    
-    let number = 0;
-  
-    while(number < maxNumber) {
-      number++;
-      yield number;
-    }
-  } catch (error) {
-    console.log('Случилась ошибка:', error)
-  }
+// function* generateNumbers(maxNumber) {
+//   try {
+//     let number = 0;
+
+//     while (number < maxNumber) {
+//       number++;
+//       yield number;
+//     }
+//   } catch (error) {
+//     console.log('Случилась ошибка:', error);
+//   }
+// }
+
+// const iter = generateNumbers(100);
+
+// for (let i = 0; i < 120; i++) {
+//   if (i === 10) {
+//     iter.return('бабах');
+//   }
+
+//   console.log(iter.next());
+// }
+
+/*
+  Каррирование - процесс уменьшения арности функции
+*/
+
+function sum(a, b, c) {
+  return a + b + c;
 }
 
-const iter = generateNumbers(100);
+console.log('sum 1 2 3:', sum(1, 2, 3));
 
-for(let i = 0; i < 120; i++) {
-
-
-  if(i === 10) {
-    iter.return('бабах');
-  }
-
-  console.log(iter.next());
-
+function curriedSum(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
 }
+
+const withFirstNumber = curriedSum(1);
+
+const withSecondNumber = withFirstNumber(2);
+
+// console.log('curried sum 1 2 3:', withSecondNumber(3));
+// console.log('curried sum но одним вызовом 1 2 3:', curriedSum(1)(2)(3));
+
+function countPrice(price, amount, discount) {
+  return price * amount - price * amount * discount;
+}
+
+console.log(countPrice(100, 2, 0.1));
+
+// function curriedPrice(price) {
+//   return function (amount) {
+//     return function (discount) {
+//       return price * amount - price * amount * discount;
+//     };
+//   };
+// }
+
+const curriedPrice = (price) => (amount) => (discount) =>
+  price * amount - price * amount * discount;
+
+const withPrice100 = curriedPrice(100);
+
+const withAmount5AndPrice100 = withPrice100(5);
+
+const result1 = withAmount5AndPrice100(0.2);
+
+const result2 = withAmount5AndPrice100(0.98);
+
+console.log(result1);
+console.log(result2);
